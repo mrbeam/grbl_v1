48ffdccc73a4e7b35d00613717d1fd9c22f1fed6
Teja <mail@tejaphilipp.de>
2015-05-01 12:16:30 +0200

    mv grbl.senior.hex from temp file to avoid the git dirty flag

--- compile.sh
+++ compile.sh
@@ -2,11 +2,12 @@
 
 echo "Compiling grbl.senior.hex"
 make clean
-make && cp grbl.hex grbl.senior.hex
+make && cp grbl.hex _grbl.senior.hex
 
 echo "Compiling grbl.junior.hex"
 make clean
 make CFLAGS=-DMRBEAM_JUNIOR && cp grbl.hex grbl.junior.hex
 
+mv _grbl.senior.hex grbl.senior.hex
 echo "done"
 
