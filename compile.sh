cea41545ce682f69d04b5a6ca430ea009f82f2ae
make-ing <bckr.florian@gmail.com>
2015-09-08 15:16:20 +0200

    changed building process to only build one hex file (senior)

--- compile.sh
+++ compile.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-echo "Compiling grbl.senior.hex"
+echo "Compiling grbl.hex..."
 make clean
-make && cp grbl.hex _grbl.senior.hex
-
-echo "Compiling grbl.junior.hex"
-make clean
-make CFLAGS=-DMRBEAM_JUNIOR && cp grbl.hex grbl.junior.hex
-
-mv _grbl.senior.hex grbl.senior.hex
+make
 echo "done"
 
